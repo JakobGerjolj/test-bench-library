11f6acf4e9c5b283fbf7bc7d1c042bf7dc2a9f83
Jakob <jakicaa111@gmail.com>
2024-04-02 12:57:21 +0200

    Second commit

--- testbenchlibrary.cpp
+++ testbenchlibrary.cpp
@@ -1,7 +1,32 @@
 #include "testbenchlibrary.h"
 
-TestBenchLibrary::TestBenchLibrary() {
+TestBenchLibrary::TestBenchLibrary()
+{
 
-//set ports
+}
 
+TestBenchLibrary::TestBenchLibrary(QSerialPort* serial_levers, QSerialPort* serial_keyCards, QString can_bus)
+{
+    m_canBusRouter= new CanBusRouter(1,"socketcan", can_bus);
+    // m_leversActuator= new LeversActuator(serial_levers);
+    // m_keyCardsActuator= new KeyCardsActuator(serial_keyCards);
+    // m_buttons = new Buttons;
+
+}
+
+void TestBenchLibrary::startReadingCan()
+{
+    m_canBusRouter->takeReceivedFrames(1);
+}
+
+void TestBenchLibrary::moveCorrectCard()
+{
+    //m_keyCardsActuator->approchRightCard();
+    qDebug()<<"Moved right card";
+}
+
+void TestBenchLibrary::moveWrongCard()
+{
+    //m_keyCardsActuator->approchLeftCard();
+    qDebug()<<"Moved wrong card";
 }
--- testbenchlibrary.h
+++ testbenchlibrary.h
@@ -1,21 +1,29 @@
 #ifndef TESTBENCHLIBRARY_H
 #define TESTBENCHLIBRARY_H
 
+#include <QDebug>
 #include "test-bench-library_global.h"
 #include "keycardsactuator.h"
 #include "leversactuator.h"
 #include "buttons.h"
+#include "canbusrouter.h"
 
 class TESTBENCHLIBRARY_EXPORT TestBenchLibrary
 {
 
 public:
-    TestBenchLibrary();
+    explicit TestBenchLibrary();
+    explicit TestBenchLibrary(QSerialPort* serial_levers,QSerialPort* serial_keyCards,QString can_bus);
+    void startReadingCan();
+    void moveCorrectCard();
+    void moveWrongCard();
 
 private:
     LeversActuator* m_leversActuator;
     KeyCardsActuator* m_keyCardsActuator;
     Buttons* m_buttons;
+    CanBusRouter* m_canBusRouter;
+    CanFrameCache* m_canFrameCache;
 
 };
 
